15d3ac87dec71bfa8ce3a92dcde4c64f14871031
menuin <menuin@hanyang.ac.kr>
2021-11-13 16:35:41 +0900

    ^_^

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,89 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import { CardModel } from '../types/model'
-import GenericCard from './GenericCard'
-import CardFactory from './CardFactory'
-import firebase from '../firebase'
-import { Firestore } from 'firebase/firestore/lite'
+import React, { useEffect, useState } from "react";
+import { CardModel } from "../types/model";
+import GenericCard from "./GenericCard";
+import CardFactory from "./CardFactory";
+import firebase from "../firebase";
+import { Firestore } from "firebase/firestore/lite";
 
 const CardContainer: React.FC = () => {
-    const [state, setState] = useState<{ data: CardModel, id: string }[]>([])
+  const [state, setState] = useState<{ data: CardModel; id: string }[]>([]);
 
-    // firestore에서 받아온 데이터로 초기 state 설정
-    const loadData = async (db: Firestore) => {
-        const collection = firebase.collection(db, 'card_collection')
-        const snapshot = await firebase.getDocs(collection)
-        const docs = snapshot.docs.map(
-            doc => ({
-                data: doc.data() as CardModel,
-                id: doc.id,
-            })
-        )
-        setState(docs)
-    }
+  // firestore에서 받아온 데이터로 초기 state 설정
+  const loadData = async (db: Firestore) => {
+    const collection = firebase.collection(db, "card_collection");
+    const snapshot = await firebase.getDocs(collection);
+    const docs = snapshot.docs.map((doc) => ({
+      data: doc.data() as CardModel,
+      id: doc.id,
+    }));
+    setState(docs);
+  };
 
-    useEffect(() => {
-        loadData(firebase.db)
-    }, [])
+  useEffect(() => {
+    loadData(firebase.db);
+  }, []);
 
-    const createCard = async (card: CardModel) => {
-        const { createdDate, contentType, src, href, content } = card
-        const newCard = {
-            createdDate,
-            contentType,
-            src : src !== undefined ? src : '',
-            href : href !== undefined ? href : '',
-            content,
-        }
+  const createCard = async (card: CardModel) => {
+    const { createdDate, contentType, src, href, content } = card;
+    const newCard = {
+      createdDate,
+      contentType,
+      src: src !== undefined ? src : "",
+      href: href !== undefined ? href : "",
+      content,
+    };
 
-        // firestore에 카드 데이터 추가
-        const collection = firebase.collection(firebase.db, 'card_collection')
-        const ref = await firebase.addDoc(collection, newCard)
-        setState([
-            ...state,
-            {
-                data: newCard,
-                id: ref.id,
-            }
-        ])
+    // firestore에 카드 데이터 추가
+    const collection = firebase.collection(firebase.db, "card_collection");
+    const ref = await firebase.addDoc(collection, newCard);
+    setState([
+      ...state,
+      {
+        data: newCard,
+        id: ref.id,
+      },
+    ]);
+  };
 
-    }
-    
-    const deleteCard = async (id: string) => {
-        setState(state.filter( card => card.id !== id ))
+  const deleteCard = async (id: string) => {
+    setState(state.filter((card) => card.id !== id));
 
-        // firestore에서 삭제
-        const collection = firebase.collection(firebase.db, 'card_collection')
-        await firebase.deleteDoc(firebase.doc(collection, id))
-    }
+    // firestore에서 삭제
+    const collection = firebase.collection(firebase.db, "card_collection");
+    await firebase.deleteDoc(firebase.doc(collection, id));
+  };
 
-    const style = {
-        margin: 'auto',
-        width: '800px',
-    }
+  const style = {
+    margin: "auto",
+    width: "800px",
+  };
 
-    return (
-        <div style={style}>
-        <CardFactory onCreate={createCard}></CardFactory>
+  return (
+    <div style={style}>
+      <CardFactory onCreate={createCard}></CardFactory>
 
-            {
-                state
-                    .sort((a,b) => b.data.createdDate - a.data.createdDate)
-                    .map((doc, index) => {
-                    return (
-                        <div>
-                        <GenericCard
-                            key = {index}
-                            id = {doc.id}
-                            data = {doc.data}
-                            deleteCard = {deleteCard}
-                        />
-                        <br/>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+      {state
+        .sort((a, b) => b.data.createdDate - a.data.createdDate)
+        .map((doc, index) => {
+          return (
+            <div>
+              <GenericCard
+                key={index}
+                id={doc.id}
+                data={doc.data}
+                deleteCard={deleteCard}
+              />
+              <br />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
-export default CardContainer
+export default CardContainer;
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { googleLogout } from '../utils/googleAuthentication'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { googleLogout } from "../utils/googleAuthentication";
 
-const LogoutButton = ({ onLogout }: { onLogout: () => void}) => {
+const LogoutButton = ({ onLogout }: { onLogout: () => void }) => {
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    googleLogout();
+    onLogout();
+  };
 
-    const handleClick: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        googleLogout();
-        onLogout();
-    }
+  return <Button onClick={handleClick}>Logout</Button>;
+};
 
-    return (
-        <Button onClick={handleClick}>logout</Button>
-    )
-}
-
-export default LogoutButton
+export default LogoutButton;
